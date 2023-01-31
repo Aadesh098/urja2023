f1cefb57dbecaf8a0d28b0c4547dd69bddd1733f
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2023-01-31 22:08:45 +0530

    Button Commit

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,7 +6,6 @@ import { images } from '../../constants';
 import './Header.css';
 
 
-
 const Header = () => {
 
   const [ref, inView] = useInView();
@@ -15,7 +14,7 @@ const Header = () => {
   <div className="app__header app__wrapper section__padding" id="home" ref={ref}>
     <div className="app__wrapper_info">
       <SubHeading title="Experience The" />
-      <h1 className={"app__header-h1 " + (inView? "sltr":"")}>Blazing Fire of the <br /> Sun</h1>
+      <h1 className={"app__header-h1 " + (inView? "sltr":"")}>Blazing Fire of the Sun</h1>
       <p className={"p__opensans " + (inView? "sltr":"")} style={{ margin: '2rem 0', animationDelay : '.5s', maxWidth: '800px', opacity: '0'}}>Unleash your potential and showcase your skills at our annual Sports Fest. From thrilling competitions to exciting events, the opportunities for greatness await you.</p>
       <button type="button" className="custom__button"><a href="#about">Explore</a></button>
     </div>

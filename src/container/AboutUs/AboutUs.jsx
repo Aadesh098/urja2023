e1398f5f81d0eb12ed0555721cabf4f3d238662a
mesaaransh <mesaaransh@gmail.com>
2023-02-04 17:31:21 +0530

    inor changess

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -17,7 +17,13 @@ const AboutUs = () => {
         <div className={inView?'srtl':""} style={{animationDelay : '1s', opacity: '0', maxWidth: '450px'}}>
           <h1 className="headtext__cormorant">About Us</h1>
           <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-          <p className="p__opensans">The Urja Club of Thapar University, a premier student-run organization, is committed to fostering a culture of athleticism within the college community. Our objective is to provide a comprehensive platform for students to exhibit their athletic prowess. We take great pride in our stewardship of the Annual Sports Fest, a highly-anticipated and competitive inter-collegiate championship that brings together the finest athletes from institutions across the region to engage in thrilling contests. Join us as we strive to cultivate a dynamic atmosphere of sportsmanship at Thapar University.</p>
+          <p className="p__opensans">
+          
+
+            Urja is one of the biggest sports extravaganzas in the Country, and We are back with the 6th edition this year. Thapar Institute of Engineering & Technology's annual sports fest has been a stage for college teams to showcase their supremacy by competing amongst the nation's best talents. TIET has had a rich history of hosting magnificent and renowned fests. The fests have become an integral part of the TIET culture and identity, encompassing innumerable institutes pan-India. After a three-year gap, the fest is set to witness the thrill, excitement, and competitiveness like never before.
+            Train yourself and get ready to go out, all guns blazing, in the quest for victory!
+            
+            </p>
           <button type="button" className="custom__button"><a href="#intro">Know More</a></button>
         </div>
       </div>
@@ -30,7 +36,7 @@ const AboutUs = () => {
         <div className={inView?'sltr':""} style={{animationDelay : '.5s', opacity: '0', maxWidth: '450px'}}>
           <h1 className="headtext__cormorant">Our History</h1>
           <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-          <p className="p__opensans">Over the years, the club has made a name for itself by organizing one of the largest and most competitive sports fests in the state. This annual event, which brings together student-athletes from institutions across the region, has become a highlight of the college calendar, drawing thousands of participants and spectators from all over the state. Through the dedicated efforts of our members, the Urja Club has established a reputation as a leader in the field of college sports, and continues to inspire and motivate students to pursue their athletic passions.</p>
+          <p className="p__opensans">Over the years, the sports fest has made a name for itself by organizing one of the largest and most competitive sports fests in the state. This annual event, which brings together student-athletes from institutions across the region, has become a highlight of the college calendar, drawing thousands of participants and spectators from all over the state. Through the dedicated efforts of our members, the Urja Club has established a reputation as a leader in the field of college sports, and continues to inspire and motivate students to pursue their athletic passions.</p>
           <button type="button" className="custom__button"><a href="#intro">Know More</a></button>
         </div>
       </div>

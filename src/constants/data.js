1cfa2a03aa266a57c821b4def098880e22b1794f
Sahil-Dhillon <sd7082599@gmail.com>
2023-02-06 17:07:29 +0530

    link updates

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,53 +1,50 @@
-
-const sports1 = [
-  {
-    title: 'Football',
-    price: 'https://drive.google.com/file/d/1-0XG3rw1oEzQy81gW3Gyi3GEur3CE52u/view?usp=share_link',
-    tags: '',
-  },
-  {
-    title: 'VolleyBall',
-    price: 'https://drive.google.com/file/d/1784bdWFU3z0rvOt92aLK7gyKIh9SxLr_/view?usp=share_link',
-    tags: '',
-  },
-  {
-    title: 'BasketBall',
-    price: 'https://drive.google.com/file/d/1QimBUpfoW5hpdIBp3LPofvhV92JWoA3a/view?usp=share_link',
-    tags: '',
-  },
-  {
-    title: 'Cricket',
-    price: 'https://drive.google.com/file/d/1KvUkCa9undE3MevzUEX25OJrHwqgs78q/view?usp=share_link',
-    tags: '',
-  },
-  {
-    title: 'Athletics',
-    price: 'https://drive.google.com/file/d/1lKXC0L9GvVcXBodtCEdr70xIMv5eGN2U/view?usp=share_link',
-    tags: '',
-  },
+const sports1 = [{
+        title: 'Football',
+        price: 'https://drive.google.com/file/d/1-0XG3rw1oEzQy81gW3Gyi3GEur3CE52u/view?usp=share_link',
+        tags: '',
+    },
+    {
+        title: 'VolleyBall',
+        price: 'https://drive.google.com/file/d/1784bdWFU3z0rvOt92aLK7gyKIh9SxLr_/view?usp=share_link',
+        tags: '',
+    },
+    {
+        title: 'BasketBall',
+        price: 'https://drive.google.com/file/d/1QimBUpfoW5hpdIBp3LPofvhV92JWoA3a/view?usp=share_link',
+        tags: '',
+    },
+    {
+        title: 'Cricket',
+        price: 'https://drive.google.com/file/d/1joVpnbsdt8vGuiCKPJmHejJZPuh0Y2_i/view?usp=share_link',
+        tags: '',
+    },
+    {
+        title: 'Athletics',
+        price: 'https://drive.google.com/file/d/1lKXC0L9GvVcXBodtCEdr70xIMv5eGN2U/view?usp=share_link',
+        tags: '',
+    },
 ];
 
-const sports2 = [
-  {
-    title: 'Table Tennis',
-    price: 'https://drive.google.com/file/d/1YF9fDXITDCe2WgiWvrlS33NLcvoHZXtL/view?usp=share_link',
-    tags: '',
-  },
-  {
-    title: "Chess",
-    price: 'https://drive.google.com/file/d/1QimBUpfoW5hpdIBp3LPofvhV92JWoA3a/view?usp=share_link',
-    tags: '',
-  },
-  {
-    title: 'Badminton',
-    price: 'https://drive.google.com/file/d/1_VAPsWBocSshQlXbHmm1BYY_DFEVla-W/view?usp=share_link',
-    tags: '',
-  },
-  {
-    title: 'Tennis',
-    price: 'https://drive.google.com/file/d/1cLJ5QvAY8bcSmWRTDiuYDs4Gd_ABnWqR/view?usp=share_link',
-    tags: '',
-  },
+const sports2 = [{
+        title: 'Table Tennis',
+        price: 'https://drive.google.com/file/d/1YF9fDXITDCe2WgiWvrlS33NLcvoHZXtL/view?usp=share_link',
+        tags: '',
+    },
+    {
+        title: "Chess",
+        price: 'https://drive.google.com/file/d/1QimBUpfoW5hpdIBp3LPofvhV92JWoA3a/view?usp=share_link',
+        tags: '',
+    },
+    {
+        title: 'Badminton',
+        price: 'https://drive.google.com/file/d/1_VAPsWBocSshQlXbHmm1BYY_DFEVla-W/view?usp=share_link',
+        tags: '',
+    },
+    {
+        title: 'Tennis',
+        price: 'https://drive.google.com/file/d/1cLJ5QvAY8bcSmWRTDiuYDs4Gd_ABnWqR/view?usp=share_link',
+        tags: '',
+    },
 ];
 // eslint-disable-next-line 
 export default { sports1, sports2 };

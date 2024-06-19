5527453af3dfa7d6f5d99e5b3841fa6b5e2948f3
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-06-19 16:20:33 +0530

    Update data.js

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,48 +1,48 @@
 const sports1 = [{
         title: 'Football',
-        price: 'https://drive.google.com/file/d/1-0XG3rw1oEzQy81gW3Gyi3GEur3CE52u/view?usp=share_link',
+        price: 'https://drive.google.com/file/d/1_VAPsWBocSshQlXbHmm1BYY_DFEVla-W/view',
         tags: '',
     },
     {
         title: 'VolleyBall',
-        price: 'https://drive.google.com/file/d/1784bdWFU3z0rvOt92aLK7gyKIh9SxLr_/view?usp=share_link',
+        price: 'https://drive.google.com/file/d/1_VAPsWBocSshQlXbHmm1BYY_DFEVla-W/view',
         tags: '',
     },
     {
         title: 'BasketBall',
-        price: 'https://drive.google.com/file/d/1QimBUpfoW5hpdIBp3LPofvhV92JWoA3a/view?usp=share_link',
+        price: 'https://drive.google.com/file/d/1_VAPsWBocSshQlXbHmm1BYY_DFEVla-W/view',
         tags: '',
     },
     {
         title: 'Cricket',
-        price: 'https://drive.google.com/file/d/1joVpnbsdt8vGuiCKPJmHejJZPuh0Y2_i/view?usp=share_link',
+        price: 'https://drive.google.com/file/d/1_VAPsWBocSshQlXbHmm1BYY_DFEVla-W/view',
         tags: '',
     },
     {
         title: 'Athletics',
-        price: 'https://drive.google.com/file/d/1lKXC0L9GvVcXBodtCEdr70xIMv5eGN2U/view?usp=share_link',
+        price: 'https://drive.google.com/file/d/1_VAPsWBocSshQlXbHmm1BYY_DFEVla-W/view',
         tags: '',
     },
 ];
 
 const sports2 = [{
         title: 'Table Tennis',
-        price: 'https://drive.google.com/file/d/1YF9fDXITDCe2WgiWvrlS33NLcvoHZXtL/view?usp=share_link',
+        price: 'https://drive.google.com/file/d/1_VAPsWBocSshQlXbHmm1BYY_DFEVla-W/view',
         tags: '',
     },
     {
         title: "Chess",
-        price: 'https://drive.google.com/file/d/1KM-NFLEJFJfUysSPt1_oe8HjnviAv3EU/view?usp=share_link',
+        price: 'https://drive.google.com/file/d/1_VAPsWBocSshQlXbHmm1BYY_DFEVla-W/view',
         tags: '',
     },
     {
         title: 'Badminton',
-        price: 'https://drive.google.com/file/d/1_VAPsWBocSshQlXbHmm1BYY_DFEVla-W/view?usp=share_link',
+        price: 'https://drive.google.com/file/d/1_VAPsWBocSshQlXbHmm1BYY_DFEVla-W/view',
         tags: '',
     },
     {
         title: 'Tennis',
-        price: 'https://drive.google.com/file/d/1cLJ5QvAY8bcSmWRTDiuYDs4Gd_ABnWqR/view?usp=share_link',
+        price: 'https://drive.google.com/file/d/1_VAPsWBocSshQlXbHmm1BYY_DFEVla-W/view',
         tags: '',
     },
 ];
